3ae978f3e86eea5179279ff1919a99484b1ac657
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-12 13:47:17 +0630

    product_api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\admin\SizeController;
+use App\Http\Controllers\admin\TempImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +18,8 @@ Route::group(['middleware'=>'auth:sanctum'],function(){
     //   Route::delete('categories/{id}',[CategoryController::class, 'destroy']);
     Route::resource('categories',CategoryController::class);//php artisan route:list
     Route::resource('brands',BrandController::class);
-
+    Route::get('sizes',[SizeController::class, 'index']);
+    Route::resource('products',ProductController::class);
+    Route::post('temp-images',[TempImageController::class, 'store']);
 });
 
