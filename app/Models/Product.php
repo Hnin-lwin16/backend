70f7a216e93d54610b5bff127b6dba8ae709b20b
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-20 21:54:21 +0630

    product api edited

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,4 +19,17 @@ class Product extends Model
         'short_description',
         'status',
     ];
+    protected $appends = ['image_url'];
+    public function getImageUrlAttribute(){
+        if($this->image == ""){
+            return "";
+        }
+        return asset('/uploads/products/small/'.$this->image);
+    }
+    function product_images(){
+        return $this->hasMany(ProductImage::class);
+    }
+    function product_sizes(){
+        return $this->hasMany(ProductSize::class);
+    } 
 }
