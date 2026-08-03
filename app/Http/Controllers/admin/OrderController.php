b346d2a6b62be1c423d9397184016a1e69abb66c
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-08-03 15:08:58 +0630

    checkout

---
+++ app/Http/Controllers/admin/OrderController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::orderBy('created_at','desc')->get();
+       return response()->json([
+        'status' => 200,
+        'data'=>$orders
+       ],200);
+    }
+   
+    public function detail($id)
+    {
+        $order = Order::with('items', 'items.product')->find($id);
+        if($order == null){
+            return response()->json([
+                'status' => 404,
+                'message'=>"Order not found"
+               ],404);
+        }
+       
+            return response()->json([
+                'status' => 200,
+                'data'=>$order
+               ],200);
+        
+    }
+    public function updateOrder($id,Request $request){
+        $order = Order::find($id);
+        if($order == null){
+            return response()->json([
+                'status'=> 404,
+                'message'=> 'Order not found.'
+            ],400);
+        }
+        $order->status = $request->status;
+        $order->payment_status = $request->payment_status;
+        $order->save();
+        return response()->json([
+            'status'=>200,
+            'data'=> $order,
+            'message'=> "Order Update Successfully"
+        ],200);
+
+    }
+}
